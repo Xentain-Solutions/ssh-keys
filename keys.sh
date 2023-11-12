8fa9356a4d36e9d3c1860490145595855fecd641
Neon <neon@wltcs.us>
2023-11-11 23:29:31 -0600

    Create keys.sh

---
+++ keys.sh
@@ -0,0 +1,17 @@
+[ ! -d "~/.ssh" ] && mkdir ~/.ssh
+cd ~/.ssh
+# Mythical Kitten 1
+echo "sk-ssh-ed25519@openssh.com AAAAGnNrLXNzaC1lZDI1NTE5QG9wZW5zc2guY29tAAAAIID43YH1tfu6AZfddca0ZwI4WT7Vt6zQkfIgb/NJAMJ/AAAAC3NzaDp0ZXJtaXVz mythical+primarykey@mythicalkitten.com" >> ~/.ssh/authorized_keys
+# Mythical Kitten 1
+sk-ssh-ed25519@openssh.com AAAAGnNrLXNzaC1lZDI1NTE5QG9wZW5zc2guY29tAAAAIAbyDRkHtQAlpgCaGuISx4RCgu1mLOZPnHLT1Vxi8Lg+AAAAC3NzaDp0ZXJtaXVz mythical+backupkey@mythicalkitten.com
+# Mythical Kitten 3
+ssh-rsa AAAAB3NzaC1yc2EAAAADAQABAAABgQDEDygWN35iG9oTCAzcz4anMt5cBKyXLqbjuOR6EOcCZNDOqQpfyxuwWfzcUM+DcxE/S62856m8ppfXF74WzCMyn+o/fpOCCpH+fqPDdDksaEOhcHende/pHZ1S8GJ3H+u+ap37q7GDeIsNedc9Uz4Dvf1NpstShd4wcj/J3dVnf+zZotRlUbqH/KEbwPcELS+CC1BxJOMQ0E5JU4rboQNocOxB8ZwVUoz0qRUQpVvrfODueWOHn22CIVnpXgALo4GWnLg9MzY33jMpuEY9ieUWyHScQx3MVkvhO5fXDGFepmr1BqflrS8nKRiIPbaQ6BGW5Qu/K9fXvj5xuPpX/Xz2m0GeKs+D5SRYiQnncvBY5ndCql2vgzHcqiBMwGFlAwwwcGtT0u8TqMKTeV2DFzzKkFTor/Wes1B0DkN9c6WbwAbgEUutasZDUFpj8zf2fBuS4Qbrk3Qj8TMPW2B2CC9SfZfc0XVtvNIZ+cq65xHUUV9h1i3pZV7BDkY1FSFBQp8= mythical@mythicalkitten.com
+# Neon Primary
+ssh-rsa AAAAB3NzaC1yc2EAAAADAQABAAABAQCBPMmBWhJMJWlJqnEObPdjbYWgFYZDNtsGFVisqOoDgIaMz/Dq2oimPytdiH69i5AwLlbvaRi6iKJK5+DmOmxZ2f9LNp949sv/7RKPbg6LxkzN5gLtC9mXxr9+oCGYuF71vVkRFhJqoOjqPJrnY0OWFE/dZ/LSFamwdxE1fZmsf6vdR4Bpx1EXg/LFGrVvtTxe7Zi9yXDDNYC2k+M2HTlbu7HNlWEjHHnNZBC1qSZwjjaRJ7r71B9pPuuF4BEvBPRF1YOAXuRTPPS9M4wnvouO5CQ3bOz6JCqbG8oypV5z1wDOFAy1RgPND63inPycYbkvoM0J+Pw5vTVyIOzsZbeh
+# Neon Backup
+ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIOZJKRSzvIDB1VJU7EWgSg/qyKDXwrLlZOdEIJE97nGF neon
+echo " "
+echo " "
+echo "Successfully added all SSH keys."
+echo " "
+echo " "
