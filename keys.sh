d27b0fa6023c824fbb78d3a469c027996542a133
Neon <neon@wltcs.us>
2023-12-16 21:23:29 -0600

    Update keys.sh

--- keys.sh
+++ keys.sh
@@ -7,10 +7,10 @@ echo "# Mythical Kitten 3" >> ~/.ssh/authorized_keys
 echo "ssh-rsa AAAAB3NzaC1yc2EAAAADAQABAAABgQDEDygWN35iG9oTCAzcz4anMt5cBKyXLqbjuOR6EOcCZNDOqQpfyxuwWfzcUM+DcxE/S62856m8ppfXF74WzCMyn+o/fpOCCpH+fqPDdDksaEOhcHende/pHZ1S8GJ3H+u+ap37q7GDeIsNedc9Uz4Dvf1NpstShd4wcj/J3dVnf+zZotRlUbqH/KEbwPcELS+CC1BxJOMQ0E5JU4rboQNocOxB8ZwVUoz0qRUQpVvrfODueWOHn22CIVnpXgALo4GWnLg9MzY33jMpuEY9ieUWyHScQx3MVkvhO5fXDGFepmr1BqflrS8nKRiIPbaQ6BGW5Qu/K9fXvj5xuPpX/Xz2m0GeKs+D5SRYiQnncvBY5ndCql2vgzHcqiBMwGFlAwwwcGtT0u8TqMKTeV2DFzzKkFTor/Wes1B0DkN9c6WbwAbgEUutasZDUFpj8zf2fBuS4Qbrk3Qj8TMPW2B2CC9SfZfc0XVtvNIZ+cq65xHUUV9h1i3pZV7BDkY1FSFBQp8= mythical@mythicalkitten.com" >> ~/.ssh/authorized_keys
 echo "# Neon Primary" >> ~/.ssh/authorized_keys
 echo "ssh-rsa AAAAB3NzaC1yc2EAAAADAQABAAABAQCBPMmBWhJMJWlJqnEObPdjbYWgFYZDNtsGFVisqOoDgIaMz/Dq2oimPytdiH69i5AwLlbvaRi6iKJK5+DmOmxZ2f9LNp949sv/7RKPbg6LxkzN5gLtC9mXxr9+oCGYuF71vVkRFhJqoOjqPJrnY0OWFE/dZ/LSFamwdxE1fZmsf6vdR4Bpx1EXg/LFGrVvtTxe7Zi9yXDDNYC2k+M2HTlbu7HNlWEjHHnNZBC1qSZwjjaRJ7r71B9pPuuF4BEvBPRF1YOAXuRTPPS9M4wnvouO5CQ3bOz6JCqbG8oypV5z1wDOFAy1RgPND63inPycYbkvoM0J+Pw5vTVyIOzsZbeh" >> ~/.ssh/authorized_keys
-echo "# Neon Backup" >> ~/.ssh/authorized_keys
-echo "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIOZJKRSzvIDB1VJU7EWgSg/qyKDXwrLlZOdEIJE97nGF neon" >> ~/.ssh/authorized_keys
-echo "# Neon Fido" >> ~/.ssh/authorized_keys
-echo "sk-ssh-ed25519@openssh.com AAAAGnNrLXNzaC1lZDI1NTE5QG9wZW5zc2guY29tAAAAIEBSGpeUxUn15Lv1E7waYo5ORLVk4PS9faiZTeV/dYIUAAAAC3NzaDp0ZXJtaXVz neon" >> ~/.ssh/authorized_keys
+echo "# Neon Backup NEW" >> ~/.ssh/authorized_keys
+echo "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAICmW8DGqLk0X3+Tth0jC/oQwWrVNBu1Vt6CrUb/wfOAU neon" >> ~/.ssh/authorized_keys
+echo "# Neon Fido NEW" >> ~/.ssh/authorized_keys
+echo "sk-ssh-ed25519@openssh.com AAAAGnNrLXNzaC1lZDI1NTE5QG9wZW5zc2guY29tAAAAIBPx+MAPuE4uzmr66zpd4HsB+TwLI78/3v9KOZMLpX9bAAAAC3NzaDp0ZXJtaXVz Neon" >> ~/.ssh/authorized_keys
 echo "# Plactrix Primary" >> ~/.ssh/authorized_keys
 echo "ssh-rsa AAAAB3NzaC1yc2EAAAADAQABAAABgQDb2OrJyyIIzXQNJamg9Xp1/t4qusN+BO6EdNpu/8CcakTrcDXWzKnNriaJTX1sBO3+xM8akSKkcTYoryZPVfNk6C2PXfO5DqDCgD09yYbcuY5PeR+h1j5PzYq45sLhnUh598JjzmOec+a1yUEVc7MirwPh0TeQgzFWzW4pXNFFhzkG1m2cmyhsTI/n6/rJGZEUO3b6fb5lC0uZgZcUg+VJq4Bfj67XlNhHVpl+loGVaO0eqAR8p5NAAvb2gSzNlJ95X7UVN9gKmByYgjkfet/DNWYo3iNnEfV3Ndlft4iuomDG/mooK+sakxBPJUhy/S7QFj1yXKEdA97M9vVrJiQaQMjNERC8zrhtlfruyHb/TFg6fehazdFLoo/CaSvn9J0v2UGbiNSi3Jq+d9Zz4hVLA4azUiRFxzig/mbwmplo3yETdY6QaBCWLnV1kk++YwZmHmTJ/UBTGz6UXVafBr+5dB6LLMKcEg/z1JFrTCHRbm23PP4ej53e1jImZv8YZZM= ramdo@DESKTOP-KAELAN" >> ~/.ssh/authorized_keys
 echo "# Plactrix Backup" >> ~/.ssh/authorized_keys
